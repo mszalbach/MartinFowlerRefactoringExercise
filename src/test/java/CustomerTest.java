9c3a0a36c90ef1910a283419edcfd79a4225cd2c
Marcel Szalbach <marcel.szalbach@googlemail.com>
2019-11-14 18:39:19 +0100

    - added some tests  - updated maven compiler plugin

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertThat;
 public class CustomerTest {
 
     @Test
-    public void testCustomer() {
+    public void testStatement() {
         Movie legoMovie = new Movie( "The Lego Movie", Movie.CHILDRENS );
         Movie watchmenMovie = new Movie( "Watchmen", Movie.REGULAR );
 
@@ -18,7 +18,31 @@ public class CustomerTest {
         customer.addRental( new Rental( watchmenMovie, 3 ) );
 
         assertThat( customer.statement(),
-                    is( "Rental Record for Test\n\tThe Lego Movie\t1.5\n\tWatchmen\t3.5\nAmount owed is 5.0\nYou earned 2 frequent renter points" ) );
+                is("Rental Record for Test\n\tThe Lego Movie\t1.5\n\tWatchmen\t3.5\nAmount owed is 5.0\nYou earned 2 frequent renter points") );
+
+    }
+
+    @Test
+    public void testStatement2() {
+        Movie frozenMovie = new Movie("Frozen II", Movie.NEW_RELEASE );
+
+        Customer customer = new Customer("Test" );
+        customer.addRental( new Rental( frozenMovie, 3 ) );
+
+        assertThat( customer.statement(),
+                is("Rental Record for Test\n\tFrozen II\t9.0\nAmount owed is 9.0\nYou earned 2 frequent renter points") );
+
+    }
+
+    @Test
+    public void testStatement3() {
+        Movie frozenMovie = new Movie("Frozen II", Movie.NEW_RELEASE);
+
+        Customer customer = new Customer("Test");
+        customer.addRental( new Rental(frozenMovie, 1) );
+
+        assertThat( customer.statement(),
+                is("Rental Record for Test\n\tFrozen II\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points") );
 
     }
 
